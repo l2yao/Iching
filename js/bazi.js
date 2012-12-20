c39c9bb5f2ff4bc5994c03f7a59fa19b03f2a4b2
l2yao <yl1984108@hotmail.com>
2012-12-20 12:03:54 -0500

    add year month day time display

--- js/bazi.js
+++ js/bazi.js
@@ -20,6 +20,16 @@ $(document).ready(function() {
     });
 
     $('#getBazi').click(function(){
-
+        var ob = new Object();
+        var t = timeStr2hour($('#input-time').attr('value'));
+        var jd = JD.JD(year2Ayear(JD.Y), JD.M, JD.D + t/24);
+        obb.mingLiBaZi(jd + curTZ/24-J2000, $('#input-longitude').attr('value')/radd, ob );
+        
+        var bazi = '<table class="table"><tbody><tr class="info"><td><h2>'+ob.bz_jn+'年</h2></td><td><h2>'+ob.bz_jy+'月</h2></td><td><h2>'+ob.bz_jr+'日</h2></td><td><h2>'+ob.bz_js+'时</h2></td></tr></tbody>';
+        bazi += '</table>';
+        $('#guaxiang').html(bazi);
     });
+    
+    $('#getCurlocation').click();
+    $('#getCurtime').click();
 });
