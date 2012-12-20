4687c410b1c5c1cf248fc9f220fb6d111acecb75
l2yao <yl1984108@hotmail.com>
2012-12-20 10:11:46 -0500

    make curTime and curLocation work

--- js/bazi.js
+++ js/bazi.js
@@ -1,23 +1,25 @@
-$('#getCurlocation').click(function(){
-    if (navigator.geolocation)
-    {
-        navigator.geolocation.getCurrentPosition(function(position){
-            $('#input-latitude').value = position.coords.latitude;
-            $('#input-longitude').value = position.coords.longitude;
-        });
-    }
-});
+$(document).ready(function() {
+    $('#getCurlocation').click(function(){
+        if (navigator.geolocation)
+        {
+            navigator.geolocation.getCurrentPosition(function(position){
+                $('#input-latitude').attr('value', position.coords.latitude);
+                $('#input-longitude').attr('value', position.coords.longitude);
+            });
+        }
+    });
 
-$('#getCurtime').click(function(){
- var now=new Date();
- curTZ = now.getTimezoneOffset()/60; //时区 -8为北京时
- curJD = now/86400000-10957.5 - curTZ/24; //J2000起算的儒略日数(当前本地时间)
- JD.setFromJD(curJD+J2000);
+    $('#getCurtime').click(function(){
+     var now=new Date();
+     curTZ = now.getTimezoneOffset()/60; //时区 -8为北京时
+     curJD = now/86400000-10957.5 - curTZ/24; //J2000起算的儒略日数(当前本地时间)
+     JD.setFromJD(curJD+J2000);
 
- $('#input-date').attr('value', JD.Y+'-'+JD.M+'-'+JD.D);
- $('#input-time').attr('value', JD.h+':'+JD.m);
-});
+     $('#input-date').attr('value', JD.Y+'-'+JD.M+'-'+JD.D);
+     $('#input-time').attr('value', JD.h+':'+JD.m);
+    });
 
-$('#getBazi').click(function(){
+    $('#getBazi').click(function(){
 
+    });
 });
