43935350f462ee48e2f60efa3f217d376db2b4e6
l2yao <yl1984108@hotmail.com>
2012-12-31 23:08:49 -0500

    add na jia to gua xiang

--- js/iching.js
+++ js/iching.js
@@ -75,12 +75,157 @@ var Iching = (function() {
             var lower_index = Math.floor(hexagram_index / 8);
             var upper_index = hexagram_index % 8;
             Iching.drawBagua(paper, Iching.Trigram[upper_index], 50, 50, 50, 200, 30);
+            Iching.drawNaJia(paper, Iching.Trigram[upper_index], 'upper', 20, 65, 50);
             Iching.drawBagua(paper, Iching.Trigram[lower_index], 50, 210, 50, 200, 30);
+            Iching.drawNaJia(paper, Iching.Trigram[lower_index], 'lower', 20, 225, 50);
         },
         drawTrigrams : function(div, upper_trigram, lower_trigram) {
             var paper = Raphael(div, 300, 420);
             Iching.drawBagua(paper, upper_trigram, 50, 50, 50, 200, 30);
+            Iching.drawNaJia(paper, upper_trigram, 'upper', 20, 65, 50);
             Iching.drawBagua(paper, lower_trigram, 50, 210, 50, 200, 30);
+            Iching.drawNaJia(paper, lower_trigram, 'lower', 20, 225, 50);
+        },
+        drawNaJia : function(paper, trigram, gua_pos, x, y, interval) {
+            switch(trigram){
+            case '☰':
+                if(gua_pos === 'lower'){
+                    var text = paper.text(x, y, '辰土');
+                    text.attr({'font-size':16});
+                    text = paper.text(x, y+interval, '寅木');
+                    text.attr({'font-size':16});
+                    text = paper.text(x, y+interval*2, '子水');
+                    text.attr({'font-size':16});
+                }else if(gua_pos === 'upper'){
+                    var text = paper.text(x, y, '戌土');
+                    text.attr({'font-size':16});
+                    text = paper.text(x, y+interval, '申金');
+                    text.attr({'font-size':16});
+                    text = paper.text(x, y+interval*2, '午火');
+                    text.attr({'font-size':16});
+                }
+                break;
+            case '☱':
+                if(gua_pos === 'lower'){
+                    var text = paper.text(x, y, '丑土');
+                    text.attr({'font-size':16});
+                    text = paper.text(x, y+interval, '卯木');
+                    text.attr({'font-size':16});
+                    text = paper.text(x, y+interval*2, '巳火');
+                    text.attr({'font-size':16});
+                }else if(gua_pos === 'upper'){
+                    var text = paper.text(x, y, '未土');
+                    text.attr({'font-size':16});
+                    text = paper.text(x, y+interval, '酉金');
+                    text.attr({'font-size':16});
+                    text = paper.text(x, y+interval*2, '亥水');
+                    text.attr({'font-size':16});
+                }
+                break;
+            case '☲':
+                if(gua_pos === 'lower'){
+                    var text = paper.text(x, y, '亥水');
+                    text.attr({'font-size':16});
+                    text = paper.text(x, y+interval, '丑土');
+                    text.attr({'font-size':16});
+                    text = paper.text(x, y+interval*2, '卯木');
+                    text.attr({'font-size':16});
+                }else if(gua_pos === 'upper'){
+                    var text = paper.text(x, y, '巳火');
+                    text.attr({'font-size':16});
+                    text = paper.text(x, y+interval, '未土');
+                    text.attr({'font-size':16});
+                    text = paper.text(x, y+interval*2, '酉金');
+                    text.attr({'font-size':16});
+                }
+                break;
+            case '☳':
+                if(gua_pos === 'lower'){
+                    var text = paper.text(x, y, '辰土');
+                    text.attr({'font-size':16});
+                    text = paper.text(x, y+interval, '寅木');
+                    text.attr({'font-size':16});
+                    text = paper.text(x, y+interval*2, '子水');
+                    text.attr({'font-size':16});
+                }else if(gua_pos === 'upper'){
+                    var text = paper.text(x, y, '戌土');
+                    text.attr({'font-size':16});
+                    text = paper.text(x, y+interval, '申金');
+                    text.attr({'font-size':16});
+                    text = paper.text(x, y+interval*2, '午火');
+                    text.attr({'font-size':16});
+                }
+                break;
+            case '☴':
+                if(gua_pos === 'lower'){
+                    var text = paper.text(x, y, '酉金');
+                    text.attr({'font-size':16});
+                    text = paper.text(x, y+interval, '亥水');
+                    text.attr({'font-size':16});
+                    text = paper.text(x, y+interval*2, '丑土');
+                    text.attr({'font-size':16});
+                }else if(gua_pos === 'upper'){
+                    var text = paper.text(x, y, '卯木');
+                    text.attr({'font-size':16});
+                    text = paper.text(x, y+interval, '巳火');
+                    text.attr({'font-size':16});
+                    text = paper.text(x, y+interval*2, '未土');
+                    text.attr({'font-size':16});
+                }
+                break;
+            case '☵':
+                if(gua_pos === 'lower'){
+                    var text = paper.text(x, y, '午火');
+                    text.attr({'font-size':16});
+                    text = paper.text(x, y+interval, '辰土');
+                    text.attr({'font-size':16});
+                    text = paper.text(x, y+interval*2, '寅木');
+                    text.attr({'font-size':16});
+                }else if(gua_pos === 'upper'){
+                    var text = paper.text(x, y, '子水');
+                    text.attr({'font-size':16});
+                    text = paper.text(x, y+interval, '戌土');
+                    text.attr({'font-size':16});
+                    text = paper.text(x, y+interval*2, '申金');
+                    text.attr({'font-size':16});
+                }
+                break;
+            case '☶':
+                if(gua_pos === 'lower'){
+                    var text = paper.text(x, y, '申金');
+                    text.attr({'font-size':16});
+                    text = paper.text(x, y+interval, '午火');
+                    text.attr({'font-size':16});
+                    text = paper.text(x, y+interval*2, '辰土');
+                    text.attr({'font-size':16});
+                }else if(gua_pos === 'upper'){
+                    var text = paper.text(x, y, '寅木');
+                    text.attr({'font-size':16});
+                    text = paper.text(x, y+interval, '子水');
+                    text.attr({'font-size':16});
+                    text = paper.text(x, y+interval*2, '戌土');
+                    text.attr({'font-size':16});
+                }
+                break;
+            case '☷':
+                if(gua_pos === 'lower'){
+                    var text = paper.text(x, y, '卯木');
+                    text.attr({'font-size':16});
+                    text = paper.text(x, y+interval, '巳火');
+                    text.attr({'font-size':16});
+                    text = paper.text(x, y+interval*2, '未土');
+                    text.attr({'font-size':16});
+                }else if(gua_pos === 'upper'){
+                    var text = paper.text(x, y, '酉金');
+                    text.attr({'font-size':16});
+                    text = paper.text(x, y+interval, '亥水');
+                    text.attr({'font-size':16});
+                    text = paper.text(x, y+interval*2, '丑土');
+                    text.attr({'font-size':16});
+                }
+                break;
+            }
+
         }
     }
     return iching;
