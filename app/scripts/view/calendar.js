8d7332b5d712e59b7dafa7f9fa36c766a57d98ed
l2yao <yl1984108@hotmail.com>
2013-03-17 22:38:08 -0400

    remove vml js

--- app/scripts/view/calendar.js
+++ app/scripts/view/calendar.js
@@ -1,5 +1,5 @@
 define(['jquery','underscore','backbone', 'handlebars','hbs!../../templates/calendar','iching','bootstraptab',
-    'lunar/lunar','lunar/eph','lunar/ephB','lunar/calendar'], 
+    'lunar/lunar','lunar/eph','lunar/eph0','lunar/ephB','lunar/page_gj','lunar/calendar'], 
     function ($, _, Backbone,Handlebars,templOne) {
     'use strict';
 
@@ -12,6 +12,9 @@ define(['jquery','underscore','backbone', 'handlebars','hbs!../../templates/cale
             var self = this;
             var html = self.template();
             self.$el.html(html);
+
+            // render this month calendar
+            changeMonth(2);
         }
     });
 
