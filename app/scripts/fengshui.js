390f7d7dfc995a98fbaf9b10c3042c512a202667
l2yao <yl1984108@hotmail.com>
2014-05-23 20:59:26 -0400

    add fei xing pan

--- app/scripts/fengshui.js
+++ app/scripts/fengshui.js
@@ -7,7 +7,7 @@ var fengshui = {
 	tianyuanlong: ['乾','坤','艮','巽','子','午','卯','酉'],
 	diyuanlong: ['甲','庚','壬','丙','辰','戌','丑','未'],
 	renyuanlong: ['寅','申','巳','亥','癸','丁','乙','辛'],
-	yuanyunpan: [[1,2],[2,0],[0,1],[0,0],[1,1],[2,2],[2,1],[0,2],[1,0]],
+	yuanyunpan: [7,2,3,0,4,8,5,6,1],
 
 	getMountain: function(degree){
 		var index = Math.floor((degree + 7.5) % 360 / 15);
@@ -19,9 +19,55 @@ var fengshui = {
 		return this.mountains[(index+12) % 24];
 	},
 
+	getYun: function(year) {
+		var yun = Math.floor((year - 64) % 180 / 20) + 1;
+		return yun;
+	},
+
 	getPan: function(yun) {
-		if( yun == 5){
+		var shift = yun - 5;
+		if( shift == 0){
 			return this.yuanyunpan;
+		}else if(shift < 0) {
+			var firsthalf = this.yuanyunpan.slice(0, Math.abs(shift));
+			var secondhalf = this.yuanyunpan.slice(Math.abs(shift), this.yuanyunpan.length);
+			return secondhalf.concat(firsthalf);
+		}else {
+			var firsthalf = this.yuanyunpan.slice(0, this.yuanyunpan.length - Math.abs(shift));
+			var secondhalf = this.yuanyunpan.slice(this.yuanyunpan.length - Math.abs(shift), this.yuanyunpan.length);
+			return secondhalf.concat(firsthalf);
 		}
+	},
+
+	drawPan: function(div, yun) {
+		var pan = this.getPan(yun);
+
+		d3.select("svg").remove();
+		var svgContainer = d3.select(div).append("svg")
+                                 .attr("width", 450)
+                                 .attr("height", 450);
+
+        for(var x = 0; x<=450; x+= 150){
+        	for(var y =0; y<=450; y+= 150){
+        		svgContainer.append("rect")
+                        .attr("x", x)
+                        .attr("y", y)
+                        .attr("width", 150)
+                        .attr("height", 150)
+                        .attr('fill', 'rgba(0,0,0,0)')
+                        .attr('stroke', 'black')
+                        .attr('stroke-width', 3);
+        	}
+        }
+        for(var i=0; i<pan.length; i++){
+        	var pos = pan[i];
+        	var x = pos % 3;
+        	var y = Math.floor(pos / 3);
+        	svgContainer.append("text")
+        				.text(i+1)
+        				.attr('x', x * 150 + 150/2 - 18/2)
+                        .attr('y', y * 150 + 150/2 + 18/2)
+                        .attr('font-size', 36);
+        }
 	}
 };
