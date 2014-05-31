2b43b5c2f193ba86647474eae745363a023de6a4
l2yao <yl1984108@hotmail.com>
2014-05-31 10:56:58 -0400

    fix hour for zhanbu

--- dist/scripts/controllers/zhanbu.js
+++ dist/scripts/controllers/zhanbu.js
@@ -24,32 +24,35 @@ angular.module('ichingApp')
 
   	$scope.getZhanBu = function(){
   		var now=new Date();
-		$scope.sizhu = new Object();
-        curTZ = now.getTimezoneOffset()/60; //时区 -8为北京时
-        curJD = now/86400000-10957.5 - curTZ/24; //J2000起算的儒略日数(当前本地时间)
-        JD.setFromJD(curJD+J2000);
+		  $scope.sizhu = new Object();
+      JD.Y = now.getFullYear();
+      JD.M = now.getMonth() + 1;
+      JD.D = now.getDate();
+      JD.h = now.getHours();
+      JD.m = now.getMinutes();
+      JD.s = now.getSeconds();
 
-        var t = now.getHours();
-        var jd = JD.JD(year2Ayear(JD.Y), JD.M, JD.D + t/24);
-        obb.mingLiBaZi(jd + curTZ/24-J2000, $scope.longitude/radd, $scope.sizhu );
+      var t = JD.h + JD.m/60 + JD.s/3600 + 4;;
+      var jd = JD.JD(year2Ayear(JD.Y), JD.M, JD.D + t/24);
+      obb.mingLiBaZi(jd -J2000, $scope.longitude/radd, $scope.sizhu );
 
-        var first_yao = iching.zheng2yinyang(parseInt($scope.liuyao[0].up));
-        var second_yao = iching.zheng2yinyang(parseInt($scope.liuyao[1].up));
-        var third_yao = iching.zheng2yinyang(parseInt($scope.liuyao[2].up));
-        var fourth_yao = iching.zheng2yinyang(parseInt($scope.liuyao[3].up));
-        var fifth_yao = iching.zheng2yinyang(parseInt($scope.liuyao[4].up));
-        var sixth_yao = iching.zheng2yinyang(parseInt($scope.liuyao[5].up));
+      var first_yao = iching.zheng2yinyang(parseInt($scope.liuyao[0].up));
+      var second_yao = iching.zheng2yinyang(parseInt($scope.liuyao[1].up));
+      var third_yao = iching.zheng2yinyang(parseInt($scope.liuyao[2].up));
+      var fourth_yao = iching.zheng2yinyang(parseInt($scope.liuyao[3].up));
+      var fifth_yao = iching.zheng2yinyang(parseInt($scope.liuyao[4].up));
+      var sixth_yao = iching.zheng2yinyang(parseInt($scope.liuyao[5].up));
 
-        $scope.up_trigram = iching.yinyang2trigram(fourth_yao,fifth_yao,sixth_yao);
-		$scope.up_index = iching.Trigram.indexOf($scope.up_trigram);
-        $scope.up_symbol = iching.Trigram_symbol[$scope.up_index];
-        $scope.down_trigram = iching.yinyang2trigram(first_yao,second_yao,third_yao);
-        $scope.low_index = iching.Trigram.indexOf($scope.down_trigram);
-        $scope.low_symbol = iching.Trigram_symbol[$scope.low_index];
-        $scope.hexagram_symbol = iching.Hexagram_name[$scope.up_index+$scope.low_index*8];
-        $scope.gong_name = iching.getGongName(iching.Hexagram[$scope.up_index+$scope.low_index*8]);
-        $scope.gong_index = iching.Trigram_name.indexOf($scope.gong_name);
-        $scope.wuxing = iching.Trigram_wuxing[$scope.gong_index];
-        iching.drawTrigrams('#gua-zhanbu', $scope.up_trigram, $scope.down_trigram, $scope.sizhu.bz_jr.substr(0,1));
+      $scope.up_trigram = iching.yinyang2trigram(fourth_yao,fifth_yao,sixth_yao);
+	    $scope.up_index = iching.Trigram.indexOf($scope.up_trigram);
+      $scope.up_symbol = iching.Trigram_symbol[$scope.up_index];
+      $scope.down_trigram = iching.yinyang2trigram(first_yao,second_yao,third_yao);
+      $scope.low_index = iching.Trigram.indexOf($scope.down_trigram);
+      $scope.low_symbol = iching.Trigram_symbol[$scope.low_index];
+      $scope.hexagram_symbol = iching.Hexagram_name[$scope.up_index+$scope.low_index*8];
+      $scope.gong_name = iching.getGongName(iching.Hexagram[$scope.up_index+$scope.low_index*8]);
+      $scope.gong_index = iching.Trigram_name.indexOf($scope.gong_name);
+      $scope.wuxing = iching.Trigram_wuxing[$scope.gong_index];
+      iching.drawTrigrams('#gua-zhanbu', $scope.up_trigram, $scope.down_trigram, $scope.sizhu.bz_jr.substr(0,1));
   	}
 });
